af23e7fac9627412b9f7e8fd9846efc7e9ca8d25
Vijay Mehrotra <vijaymehrotra99@gmail.com>
2025-01-28 17:36:29 +0530

    Created the Todo app

---
+++ index.js
@@ -0,0 +1,41 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if(inputBox.value === ''){
+        alert("Enter something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "&#10006";
+        li.appendChild(span);
+    }
+    saveData();
+    inputBox.value = "";
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+    saveData();
+}, false);
+
+
+function saveData() {
+    localStorage.setItem("data" , listContainer.innerHTML);
+}
+
+function getData() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+getData();
